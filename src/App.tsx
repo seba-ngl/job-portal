4e7b4147f49523582da3a116e8c85d02361d46c2
GodKronos1 <62173950+GodKronos1@users.noreply.github.com>
2022-09-20 23:00:45 +0300

    JP-3:add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={logo} className="logo" alt="" />
+
+        <ul>
+          <li>Home</li>
+          <li>Find jobs</li>
+          <li>Post Jobs</li>
+        </ul>
+        <div className="wrap-buttons">
+          <button className="login">Login</button>
+          <button className="sign-up">Sign Up</button>
+        </div>
+      </div>
     </div>
   );
 }
