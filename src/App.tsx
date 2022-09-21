825fa808a8d7242d7f7c001957d5408e62b3bb0b
GodKronos1 <62173950+GodKronos1@users.noreply.github.com>
2022-09-21 23:43:29 +0300

    JP-7::added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import logo from "./assets/logo.png";
+import hero from "./assets/hero.png";
+import { BiSearch } from "react-icons/bi";
+import { GoLocation } from "react-icons/go";
 
 function App() {
   return (
@@ -15,6 +18,40 @@ function App() {
         <div className="wrap-buttons">
           <button className="login">Login</button>
           <button className="sign-up">Sign Up</button>
+        </div>
+      </div>
+      <div className="container-hero">
+        <div className="hero-text">
+          <p>Your Dream</p>
+          <p className="hero-title">Job is Waiting</p>
+          <p className="hero-title3">5000+ Jobs</p>
+          <div className="whole-search-bar">
+            <div className="search-box">
+              <button className="icon-search">
+                <BiSearch></BiSearch>
+              </button>
+              <input
+                type="text"
+                placeholder="Enter job title, keyword"
+                className="search-input"
+              ></input>
+            </div>
+            <div className="search-location">
+              <button className="icon-search">
+                <GoLocation></GoLocation>
+              </button>
+              <input
+                type="text"
+                placeholder="Search Location"
+                className="search-input-location"
+              ></input>
+            </div>
+            <button className="btn-search">Search</button>
+          </div>
+        </div>
+
+        <div className="hero-img">
+          <img src={hero} className="hero-img" alt="" />
         </div>
       </div>
     </div>
